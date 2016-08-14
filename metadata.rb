37a01162de2194858881588f0bac619d5dd52d1a
Daniel Neish <danieln@exemplartec.com>
2016-08-14 11:38:09 +1000

    Remove quotes around mail server address, add ability to set mail format

--- metadata.rb
+++ metadata.rb
@@ -2,6 +2,6 @@ name              "monit-dhn"
 maintainer        "Daniel Neish"
 maintainer_email  "daniel.neish@gmail.com"
 description       "Basic monit installation"
-version           "0.3.12"
+version           "0.3.13"
 
 supports      "ubuntu"
